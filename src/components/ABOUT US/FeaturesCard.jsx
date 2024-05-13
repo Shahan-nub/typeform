940a178bc2ae1543b1b398e61c18acf959268ad7
Shahan-nub <shahananwer786@gmail.com>
2024-05-13 15:07:21 +0530

    minor bugs fixed

--- src/components/ABOUT US/FeaturesCard.jsx
+++ src/components/ABOUT US/FeaturesCard.jsx
@@ -4,12 +4,12 @@ import { CgArrowLongRight } from "react-icons/cg";
 
 export default function FeaturesCard({ title, src, id }) {
   return (
-    <div className="flex flex-col gap-4 lg:gap-7 rounded-md my-4 lg:my-auto bg-glass lg:hover:bg-glass py-5 lg:py-9 px-4 lg:px-10 text-gray-400">
+    <div className="flex  flex-col group gap-4 lg:gap-7 rounded-md my-4 lg:my-auto bg-glass lg:bg-transparent lg:hover:bg-glass transition-all duration-500 py-5 lg:py-9 px-4 lg:px-10 text-gray-400">
       <Image src={src} alt={id} height={50} width={50}></Image>
       <h2 className="text-lg lg:text-2xl font-medium text-white">{title}</h2>
       <p className="text-sm lg:text-lg font-light">Lorem ipsum dolor sit.</p>
       <hr />
-      <div className="relative group flex gap-4 lg:gap-0 items-center">
+      <div className="relative cursor-pointer flex gap-4 lg:gap-0 items-center">
         <p className="lg:absolute lg:opacity-0 lg:group-hover:opacity-100  lg:-left-10 lg:group-hover:translate-x-10 transition-all duration-500 font-bold text-white">Read more</p>
         <CgArrowLongRight className="text-xl lg:text-4xl font-thin  lg:group-hover:translate-x-24 transition-all duration-500 "/>
       </div>
